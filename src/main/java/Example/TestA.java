80fe612802a564c561466d4f6fe516b26eb22144
srikanthmohan <43953019+srikanthmohan@users.noreply.github.com>
2018-10-09 11:36:28 +0530

    updated

--- src/main/java/Example/TestA.java
+++ src/main/java/Example/TestA.java
@@ -25,6 +25,7 @@ public class TestA {
 		System.out.println("*******************");
 		System.out.println("launching chrome browser");
 		System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
 	

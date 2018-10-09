4ff78cbd84951fb57c7452cb24a6b12d540b0fb0
srikanthmohan <srikanthmohansfdc@gmail.com>
2018-10-09 16:01:54 +0530

    Updated batch file

--- testNGBatchFile.bat
+++ testNGBatchFile.bat
@@ -1,5 +1,5 @@
-set projectLocation=E:\bhargav\com.rest
-cd E:\bhargav\com.rest
-set classpath=E:\bhargav\com.rest\target\classes;E:\bhargav\com.rest\lib\*
-java org.testng.TestNG E:\bhargav\com.rest\testng.xml
+set projectLocation=E:\bhargav\com\TestNg
+cd E:\bhargav\com\TestNg
+set classpath=E:\bhargav\com\TestNg\target\classes;E:\bhargav\com\TestNg\lib\*
+java org.testng.TestNG E:\bhargav\com\TestNg\testng.xml
 pause

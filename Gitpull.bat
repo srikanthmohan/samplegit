2a9a69c6a8db221f8ffd6e9e456ddfae89e19e70
srikanthmohan <43953019+srikanthmohan@users.noreply.github.com>
2018-10-10 14:57:57 +0530

    Delete Gitpull.bat

--- Gitpull.bat
+++
@@ -1,9 +0,0 @@
-cd E:\bhargav\com\TestNg\src\main\java\Example
-set HOME=%USERPROFILE%
-<<<<<<< HEAD
-git pull
-pause
-=======
-git pull origin master 
-pause
->>>>>>> 88214033abdce6d9f4c6e7954e5c2869b321ee7e

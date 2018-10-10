76615133f6efd5938630579ee3ea6e7830bdc921
srikanthmohan <43953019+srikanthmohan@users.noreply.github.com>
2018-10-10 18:34:13 +0530

    Update testNGBatchFile.bat

--- testNGBatchFile.bat
+++ testNGBatchFile.bat
@@ -1,4 +1,4 @@
-call gitpull.bat
+start gitpull.bat
 set projectLocation=E:\bhargav\com\TestNg
 cd E:\bhargav\com\TestNg
 set classpath=E:\bhargav\com\TestNg\target\classes;E:\bhargav\com\TestNg\lib\*

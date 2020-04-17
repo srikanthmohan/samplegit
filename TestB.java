fd2a07046cc51fae9bcf6cb08cf3b6c9d6cd166d
srikanthmohan <43953019+srikanthmohan@users.noreply.github.com>
2020-04-17 16:17:26 +0530

    Updated

---
+++ TestB.java
@@ -0,0 +1,59 @@
+package Example;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.AssertJUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.TestNG;
+
+public class TestB {
+	//Sample test added
+	WebDriver driver;  
+	@BeforeMethod	
+	@BeforeClass
+	public void setUp() {
+		System.out.println("*******************");
+		System.out.println("launching chrome browser");
+		System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void TestB() {
+		driver.navigate().to("http://www.google.com");
+		String strPageTitle = driver.getTitle();
+		System.out.println("Page title: - "+strPageTitle);
+		AssertJUnit.assertTrue(strPageTitle.equalsIgnoreCase("Google"));
+	}
+	
+	@Test
+	public void testA1() {
+		System.out.println("A3");
+	}
+	 
+	@Test
+	public void testA2() {
+		System.out.println("A4");
+	}
+
+	@AfterMethod
+	@AfterClass
+	public void tearDown() {
+		if(driver!=null) {
+			System.out.println("Closing firefox browser");
+			driver.quit();
+		}
+	}
+	
+}

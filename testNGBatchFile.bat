1c3ce79ddbbe4fa64b3092d1b5511cbb04ad2d81
srikanthmohan <43953019+srikanthmohan@users.noreply.github.com>
2020-04-17 15:54:23 +0530

    updated bat

--- testNGBatchFile.bat
+++ testNGBatchFile.bat
@@ -1,5 +1,5 @@
 set projectLocation=F:\Github\samplegit
 cd F:\Github\samplegit
-set classpath=F:\Github\samplegit\target\classes;C:\Users\Golden\eclipse-workspace\TestNG\lib\*
+set classpath=F:\Github\samplegit\target\classes;F:\Github\samplegit\lib\*
 java org.testng.TestNG F:\Github\samplegit\testng.xml
 pause

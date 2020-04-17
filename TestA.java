f6f522f7354b80106d15396a22a7105f817a0423
srikanthmohan <43953019+srikanthmohan@users.noreply.github.com>
2020-04-17 16:11:46 +0530

    Updated and added

--- TestA.java
+++ TestA.java
@@ -39,12 +39,12 @@ public class TestA {
 	
 	@Test
 	public void testA1() {
-		System.out.println("A3");
+		System.out.println("A1");
 	}
 	 
 	@Test
 	public void testA2() {
-		System.out.println("A4");
+		System.out.println("A2");
 	}
 
 	@AfterMethod
